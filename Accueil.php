f64f1a7fc1885b081a1616d7f8ff76e3010a0fbf
Valentine2411 <101174239+Valentine2411@users.noreply.github.com>
2022-04-15 15:57:00 +0200

    modifs page connexion (style + ajout inscription)

--- Accueil.php
+++ Accueil.php
@@ -1,34 +1,10 @@
 <?php include("includes/header.php") ?>
 <?php include("includes/head.php");?>
 
-<<<<<<< Updated upstream
-<html>
-
-<head>
-  <title>MyStories</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  <meta charset="utf-8" />
-  <link rel="stylesheet" href="style.css" />
-</head>
-
-<body class="accueil">
-  <?php include("includes/header.php") ?>
-  <main>
-    <div id="background">
-      <p class="titre"> Bienvenue sur MyStories ! </p>
-      <br />
-      <p>Vous trouverez sur ce site de nombreuses histoires interactives : des histoires dont vous êtes le héro ou l'héroïne ! Sélectionnez une histoire parmi celles écrites par nos membres, et plongez dans un récit sur-mesure, dont l'issue dépend de vos décisions.</p>
-      </p>
-      <div id="btn-lecture">
-        <!--J'ai appelé Liste.php dans le href pour renvoyer vers une page comme dans le TP MyMovies où on voit la liste des histoires. La page liste n'existe pas encore du coup, il faudra la créer et changer son nom si besoin-->
-        <a class="btn btn-outline-dark" href="Liste.php" role="button">Commencer la lecture !</a>
-=======
   <div class="accueil">
     <main>
-      <div id="blackground">
-        <p class="sous-titre"> sur MyStories !</p>
+      <div id="background">
+        <p class="titre"> Bienvenue sur MyStories ! </p>
         <br />
         <p>Vous trouverez sur ce site de nombreuses histoires interactives : des histoires dont vous êtes le héro ou l'héroïne ! Sélectionnez une histoire parmi celles écrites par nos membres, et plongez dans un récit sur-mesure, dont l'issue dépend de vos décisions.</p>
         </p>
@@ -36,7 +12,6 @@
           <a class="btn btn-outline-dark" href="PageAccueil.php" role="button">Commencer la lecture !</a>
         </div>
         <br />
->>>>>>> Stashed changes
       </div>
     </main>
   </div>
