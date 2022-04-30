5f69c62ac4c6d16c1885f1bde37dcc0358e38ee8
Nowmaybelater <95069235+Nowmaybelater@users.noreply.github.com>
2022-04-30 11:43:33 +0200

    hide

--- story_hide.php
+++ story_hide.php
@@ -2,17 +2,50 @@
 <?php include("includes/connect.php") ?>
 <main>
     <div id="backgroundConnexion">
-        <p class="titre_petit"> Choisissez une histoire à cacher </p>
+        <p class="titre_petit"> Suppression </p>
+
+        <?php
+        if (isset($_POST['hide'])) {
+            $story=$_POST['hide'];
+            echo $story;
+            $req="UPDATE stories SET hide='1' WHERE title=:t";
+            $requete = $bdd->prepare($req);
+            $requete->execute(array("t"=>$story));
+        }
+
+
+        ?>
+
+
+
+
         <div>
-            <?php
-            $requete = "SELECT * FROM stories"; //rajouter de quoi faire que l'utilisateur peut cacher uniquement les histoires que lui a écrites
-            $resultat = $bdd->query($requete);
-            while ($histoire = $resultat->fetch()) {
-                $valeur = $histoire["id_story"];
-            ?> <h2><a href="StorySummary.php?id=<?= $valeur ?>"><?= $histoire['title'] ?></a></h2>
-            <?php
-            }
-            ?>
+        <form method="post" action="story_hide.php">
+        <div class="well"></div>
+        <br />
+            <div class="form-group" id="centre">
+                 <select name="hide" id="hide"> 
+                    <?php
+                    $requete = "SELECT * FROM stories WHERE author=?";
+                    $resultat = $bdd->prepare($requete);
+                    $resultat->execute(array($_SESSION['login']));
+                    while ($histoire = $resultat->fetch()) {
+                        $valeur = $histoire["id_story"];
+                    ?> <option value=<?= $histoire['title'] ?> style="font-size: medium;"><?= $histoire['title'] ?></option>
+                    <?php
+                    }
+                    ?>
+                </select> 
+            </div>
+                <br/>
+                <div class="form-group" id="centre">
+                <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
+                    <button type="submit" id='button' class="btn btn-default btn-primary" pb-role="submit"><span class="glyphicon glyphicon-log-in"></span> Cacher</button>
+                </div>
+                </div>
+            </p>
+        </form>
+            
         </div>
     </div>
 </main>
