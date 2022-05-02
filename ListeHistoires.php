0dbf8e4e6d090c3cfb604d00688ad40ce347301c
Tinuviel11 <vlorsery@ensc.fr>
2022-05-02 18:13:36 +0200

    ListeHistoires

--- ListeHistoires.php
+++ ListeHistoires.php
@@ -1,8 +1,10 @@
 <?php include("includes/header.php") ?>
 <?php include("includes/connect.php") ?>
+
 <main>
     <div id="backgroundConnexion">
-        <p class="titre"> Histoires disponibles </p>
+        <p class="titre"> Mes Histoires </p>
+        <br />
         <div>
             <?php
             $requete = "SELECT * FROM stories";
@@ -11,12 +13,26 @@
                 $valeur = $histoire["id_story"];
             ?>
                 <div>
-                    <h2><a href="StorySummary.php?id=<?= $valeur ?>" class="listeHistoires"><em><?= $histoire['title'] ?></em> par <?= $histoire['author'] ?></a></h2>
+                    <h2><em><?= $histoire['title'] ?></em> par <?= $histoire['author'] ?></h2>
+                    <p><?= $histoire['summary'] ?></p>
                 </div>
+                <div id="btn-lecture">
+                    <a class="btn btn-outline-dark" href="story_modify.php" role="button">Modifier</a>
+                    &nbsp;
+                    <a class="btn btn-outline-dark" href="story_delete.php" role="button">Supprimer</a>
+                    &nbsp;
+                    <a class="btn btn-outline-dark" href="story_hide.php" role="button">Cacher</a>
+                    &nbsp;
+                    <a class="btn btn-outline-dark" href="#" role="button">Statistiques</a>
+                </div>
+                <hr />
             <?php
             }
             ?>
         </div>
+
+        <br />
         <a class="btn btn-success" id="btnHautPage" href="#top" role="button"> <i class="bi bi-caret-up-fill"></i> </a>
     </div>
 </main>
+</body>
