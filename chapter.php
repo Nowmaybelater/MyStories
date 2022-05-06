908d09ac97e88d9b1b725acb12149e76e85e79d5
Nowmaybelater <95069235+Nowmaybelater@users.noreply.github.com>
2022-05-06 16:17:06 +0200

    of::

--- chapter.php
+++ chapter.php
@@ -4,6 +4,63 @@
 <main>
     <div id=backgroundConnexion>
     <?php
+        //fonction qui met à jour les point et la mort du joueur au fur et à mesure des choix
+        function Points($id_user, $bdd, $chapter, $choice)
+        {
+            $requete11 = "SELECT * FROM points WHERE id_story = :story_id AND chapter= :chapter AND numChoice= :choice";
+            $req11 = $bdd->prepare($requete11);
+            $req11->execute(array(
+                'story_id'=> $_GET['story_id'],
+                'chapter'=> $chapter,
+                'choice'=>$choice
+            ));
+            if($req11->rowCount() ==1){
+                $ligne2=$req11->fetch();
+                $pts=$ligne2['points'];
+                $death=$ligne2['death'];
+
+                $requete8 = "SELECT * FROM player_points WHERE id_user = :usr_id AND id_story = :story_id";
+                $req8 = $bdd->prepare($requete8);
+                $req8->execute(array(
+                    'usr_id' => $id_user,
+                    'story_id'=> $_GET['story_id']
+                ));
+                if($req8->rowCount() == 1){
+                    $ligne=$req8->fetch();
+                    $points=$ligne['points'] + $pts;
+                    echo $points;
+                    $req9="UPDATE player_points SET points=$points WHERE id_user = :usr_id AND id_story = :story_id";
+                    $requete9 = $bdd->prepare($req9);
+                    $requete9->execute(array(
+                        'usr_id' => $id_user,
+                        'story_id'=> $_GET['story_id']
+                    ));
+                    $req12="UPDATE player_points SET death=$death WHERE id_user = :usr_id AND id_story = :story_id";
+                    $requete12 = $bdd->prepare($req12);
+                    $requete12->execute(array(
+                        'usr_id' => $id_user,
+                        'story_id'=> $_GET['story_id']
+                    ));
+                }
+                else{
+                    $req10= $bdd->prepare('INSERT INTO player_points (id_user, id_story, points,death) VALUES (:usr_id, :id_story, :points, :death)');
+                    $req10->execute(array(
+                        'usr_id' => $id_user,
+                        'id_story' => $_GET['story_id'],
+                        'points' => $pts,
+                        'death'=>$death
+                    ));
+                    
+                }
+                if($death==1){
+                    return 1;
+                }
+                else{
+                    return 0;
+                }
+            }
+            return 0;
+        }
 
         //récupération de l'id_user
         $requete = "SELECT * FROM user WHERE login_usr = :usr_login";
@@ -27,13 +84,13 @@
 
         <?php
         //récupération du chapitre
-        $requete = "SELECT * FROM chapters WHERE id_story = :id AND numChapter = :chap";
-        $req = $bdd->prepare($requete);
-        $req->execute(array(
+        $requete7 = "SELECT * FROM chapters WHERE id_story = :id AND numChapter = :chap";
+        $req7 = $bdd->prepare($requete7);
+        $req7->execute(array(
             'id' => $_GET['story_id'],
             'chap'=> $_GET['chapter_num']
         ));
-        $chapter = $req->fetch();
+        $chapter = $req7->fetch();
         ?>
 
         <?php
@@ -77,10 +134,15 @@
                     'previous_choice'=> 1
                 ));
                 $choix1 = $req2->fetch();                
-                ?>
-                <a class="btn btn-outline-dark" href="chapter.php?story_id=<?= $choix1['id_story']?>&chapter_num=<?= $choix1['Chapter']?>&choice_num=1" role="button"><?= $chapter['choice1']?></a>
-                &nbsp;
-                <?php
+                $failed1=Points($id_user, $bdd, $_GET['chapter_num'], 1);
+                if($failed1==1){
+                    echo "death";
+                }
+                else{
+                ?>                
+                    <a class="btn btn-outline-dark" href="chapter.php?story_id=<?= $choix1['id_story']?>&chapter_num=<?= $choix1['Chapter']?>&choice_num=1" role="button"><?= $chapter['choice1']?></a>
+                    &nbsp;
+                <?php }
             }
             //affichage du contenu du choix 2 dans un bouton cliquable
             if(!empty($chapter['choice2'])){
@@ -92,10 +154,15 @@
                     'previous_choice'=> 2
                 ));
                 $choix2 = $req3->fetch(); 
+                $failed2=Points($id_user, $bdd, $_GET['chapter_num'], 2);
+                if($failed2==1){
+                    echo "death";
+                }
+                else{
                 ?>
                 <a class="btn btn-outline-dark" href="chapter.php?story_id=<?= $choix2['id_story']?>&chapter_num=<?= $choix2['Chapter']?>&choice_num=2" role="button"><?= $chapter['choice2']?></a>
                 &nbsp;
-                <?php
+                <?php }
             }
             //affichage du contenu du choix 3 dans un bouton cliquable
             if(!empty($chapter['choice3'])){
@@ -107,10 +174,15 @@
                     'previous_choice'=> 3
                 ));
                 $choix3 = $req4->fetch();
+                $failed3=Points($id_user, $bdd, $_GET['chapter_num'], 3);
+                if($failed3==1){
+                    echo "death";
+                }
+                else{
                 ?>
                 <a class="btn btn-outline-dark" href="chapter.php?story_id=<?= $choix3['id_story']?>&chapter_num=<?= $choix3['Chapter']?>&choice_num=3" role="button"><?= $chapter['choice3']?></a>
                 &nbsp;
-                <?php
+                <?php }
             }
             ?>
         </div>
