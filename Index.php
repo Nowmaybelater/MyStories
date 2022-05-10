8c02d2506750fbe59d24c14c667be06f9962887d
Tinuviel11 <vlorsery@ensc.fr>
2022-05-10 23:11:29 +0200

    Update Index.php

--- Index.php
+++ Index.php
@@ -5,7 +5,7 @@
   <div id="backgroundConnexion">
     <p class="titre"> Bienvenue sur MyStories ! </p>
     <br />
-    <p>Vous trouverez sur ce site de nombreuses histoires interactives : des histoires dont vous êtes le héro ou l'héroïne ! Sélectionnez une histoire parmi celles écrites par nos membres, et plongez dans un récit sur-mesure, dont l'issue dépend de vos décisions.</p>
+    <p>Vous trouverez sur ce site de nombreuses histoires interactives : des histoires dont vous êtes le héros ou l'héroïne ! Sélectionnez une histoire parmi celles écrites par nos membres, et plongez dans un récit sur-mesure, dont l'issue dépend de vos décisions.</p>
     <br />
     <hr />
     <div>
