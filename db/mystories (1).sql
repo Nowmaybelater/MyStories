5ba9b04ff46989cca0bdeb1ade2b905d815910d6
Nowmaybelater <95069235+Nowmaybelater@users.noreply.github.com>
2022-05-11 11:01:49 +0200

    bdd

--- db/mystories (1).sql
+++ db/mystories (1).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 10 mai 2022 à 13:21
+-- Généré le : mer. 11 mai 2022 à 11:01
 -- Version du serveur : 10.4.24-MariaDB
--- Version de PHP : 8.1.4
+-- Version de PHP : 8.1.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,7 +33,8 @@ CREATE TABLE `advancement` (
 --
 
 INSERT INTO `advancement` (`id_usr`, `id_story`, `numChapter`, `jour`) VALUES
-(2, 1, 1, '2022-05-10');
+(1, 1, 5, '2022-05-11'),
+(2, 1, 2, '2022-05-11');
 
 -- --------------------------------------------------------
 
@@ -78,7 +79,12 @@ INSERT INTO `chapters` (`id_chapter`, `id_story`, `numChapter`, `chapterContent`
 (20, 1, 20, 'Nous montons les escaliers à toute vitesse.', 'Page suivante', NULL, NULL),
 (21, 1, 21, 'Nous trouvons un policier dans la rue qui nous dit : « Voyons les enfants, restons sérieux... Je ne mets pas les gens en prison comme ça... ». Nous retournons dans les escaliers.', 'Page suivante', NULL, NULL),
 (22, 1, 22, 'Nous appelons « Monsieur Charles, monsieur Charles !!! ». Monsieur Charles nous attend tranquillement sur le palier. Il nous dit bonjour et nous demande ce que nous faisons ici. Nous lui racontons alors notre histoire de détectives. Mais voilà que monsieur Charles se met à rire :\n« Mais Rose est mon aide-ménagère ! quand je suis malade, je prête mon panier pour qu’elle aille faire mes courses. »\nNous avons tous bien rigolé et nous avons eu droit à une très belle histoire comme tous les lundis soir.', 'FIN', NULL, NULL),
-(23, 2, 6, 'sxkq,xk', 'dc,s', '', '');
+(26, 4, 1, 'Vous êtes le Capitaine Nightshade, à la tête de l’équipage de pirates le plus craint de l’histoire. A bord du Half-Moon, un navire plus imposant encore que ceux de la flotte royale, vous sillonnez les mers à la recherche des trésors les plus précieux. Vous êtes réputé pour être cruel et sans pitié. Alors pourquoi diable la personne avec qui vous aviez rendez-vous n’est-elle toujours pas là ? Cela fait près de deux heures que vous attendez sur cette île perdue, et toujours aucun navire à l’horizon. Pour la énième fois, vous relisez la mystérieuse lettre qui s’est matérialisée la veille sur le pont de votre navire. \r\n« Si vous voulez devenir le pirate le plus riche de l’histoire, rendez-vous demain sur la plage de l’île Maudite, à l’Est, à l’heure où le soleil sera à son zénith. Venez seul.\r\nE.F»\r\nPour quelle raison quelqu’un détenant les informations sur la localisation d’un immense trésor désirerait-il le partager avec vous, la personne la plus cupide que le monde n’ait jamais porté ? Décidemment, cette histoire semble louche, sans compter qu’il vous a fallu redoubler d’efforts pour convaincre votre équipage de rester à bord du Half-Moon. Pourtant, c’est justement cette cupidité qui vous pousse à attendre quelques minutes de plus.\r\n', 'Page suivante', NULL, NULL),
+(27, 4, 2, 'Alors que vous alliez perdre patience, d’énormes nuages noirs s’accumulent dans le ciel avec un grondement sinistre. Un craquement menaçant résonne dans votre dos. Vous faites immédiatement volte-face, apercevant une silhouette se matérialiser à l’orée de la jungle. ', 'Vous lancez impulsivement votre dague en direction de l’intrus ', 'Sur vos gardes, vous gardez votre calme jusqu’à ce l’intrus révèle son visage ', NULL),
+(28, 4, 3, 'Sans que vous ne l’ayez vu venir, votre dague revient vers vous avec encore plus de force que celle que vous lui aviez donnée. Le métal érafle votre bras, déchirant le tissu de votre vêtement, puis l’arme s’enfonce jusqu’à la lame dans le tronc d’un arbre à plusieurs mètres de là. \r\n–Votre impulsivité vous tuera, capitaine Nightshade. \r\nVous pansez grossièrement la blessure avec un pan de votre chemise déchirée. En ripostant de la sorte, votre adversaire vient d’établir un rapport de force que vous brûlez déjà d’inverser. ', 'Page suivante', NULL, NULL),
+(29, 4, 4, 'Devant vous se tient une jeune femme à la beauté froide et sévère, une femme dont le visage ne vous est pas inconnu. Eleena Fade est la magicienne la plus puissante du royaume. Appartenant autrefois à la cour, elle fut bannie pour trahison et personne n’entendit plus parler d’elle. Jusqu’à aujourd’hui. \r\n–J’aurai dû me douter que vous seriez derrière tout cela, Eleena. \r\n–C’était sans compter sur mes intentions de ne vous révéler mon identité qu’au dernier moment, déclare froidement la magicienne.\r\n', 'Page suivante', NULL, NULL),
+(30, 4, 5, '–Trêves de bavardages. Pourquoi m’avoir fait venir ici si vous disposez de toutes les informations nécessaires à la récupération de ce trésor ? \r\n–Question légitime. Ne croyez pas que je n’ai pas déjà tenté de dérober ce trésor par mes propres moyens. Mais il semble que la personne qui a scellé ce trésor avait une dent contre mon peuple. Seul un être au sang humain peut ouvrir le coffre. Et pour venir à bout des pièges de cette île, il faut quelqu’un qui ne craigne pas la mort, quelqu’un pour qui l’or est une motivation suffisante. \r\n–Je vois. Et quels sont les arrangements auxquels vous avez pensé ? déclarez-vous d’un ton acerbe.\r\n–Les richesses qui composent ce trésor dépassent l’entendement, croyez-moi. Mais il n’y a en réalité qu’une seule pièce de ce trésor que je convoite : le joyau de la reine. Il a été dérobé il y a des siècles, et si je le rapporte à la cour…\r\n–Vos péchés seront effacés, terminez-vous à la place d’Eleena, dont les paroles vous semblent empreintes d’une tristesse sincère. Et vous promettez que tout le reste nous reviendra, à mon équipage et à moi ? \r\n–C’est une promesse, lâche la magicienne d’un ton grinçant, une main tendue dans votre direction. \r\n', 'Vous acceptez le marché que vous propose Eleena Fade ', 'Cette femme a trahi le roi, qui l’empêcherait de vous trahir également ? Vous refusez le marché.', NULL),
+(31, 4, 6, 'Vous serrez la main de la jeune femme : marché conclu. \r\n–Vous aurez besoin de cela si vous voulez vous en sortir vivant, explique Eleena Fade en vous tendant un épais carnet de cuir. N’oubliez pas que cette île est maudite : si j’ai réussi à m’en sortir après avoir collecté toutes ces informations, c’est uniquement grâce à mes dons de magicienne. Dons dont vous ne disposez évidemment pas. Soyez prudent, et suivez mes instructions à la lettre. Je compte sur vous Nightshade. \r\nSur ces paroles ni vraiment rassurantes ni vraiment encourageantes, la magicienne se volatilisa. \r\n', 'FIN', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -98,10 +104,10 @@ CREATE TABLE `choices` (
 --
 
 INSERT INTO `choices` (`id_usr`, `id_story`, `numChapter`, `choice`) VALUES
-(2, 1, 1, 1),
-(2, 1, 2, 1),
-(2, 1, 2, 1),
-(2, 1, 3, 1);
+(1, 1, 1, 1),
+(1, 1, 2, 2),
+(1, 1, 4, 1),
+(2, 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -136,7 +142,7 @@ INSERT INTO `links` (`id_story`, `Chapter`, `Previous_Chapter`, `Previous_Choice
 (1, 10, 9, 1),
 (1, 11, 9, 2),
 (1, 12, 10, 1),
-(1, 13, 10, 1),
+(1, 13, 10, 2),
 (1, 1, 11, 1),
 (1, 14, 12, 1),
 (1, 15, 12, 2),
@@ -152,9 +158,12 @@ INSERT INTO `links` (`id_story`, `Chapter`, `Previous_Chapter`, `Previous_Choice
 (1, 20, 19, 2),
 (1, 22, 20, 1),
 (1, 22, 21, 1),
-(2, 2, 6, 1),
-(2, 0, 6, 2),
-(2, 0, 6, 3);
+(4, 2, 1, 1),
+(4, 3, 2, 1),
+(4, 4, 2, 2),
+(4, 4, 3, 1),
+(4, 5, 4, 1),
+(4, 6, 5, 1);
 
 -- --------------------------------------------------------
 
@@ -168,6 +177,13 @@ CREATE TABLE `player_points` (
   `points` int(11) DEFAULT NULL,
   `death` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `player_points`
+--
+
+INSERT INTO `player_points` (`id_user`, `id_story`, `points`, `death`) VALUES
+(1, 1, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -196,9 +212,9 @@ INSERT INTO `points` (`id_story`, `chapter`, `numChoice`, `points`, `death`) VAL
 (1, 17, 2, 1, 0),
 (1, 18, 1, 0, 1),
 (1, 19, 1, 1, 0),
-(2, 6, 1, 0, 0),
-(2, 6, 2, 0, 0),
-(2, 6, 3, 0, 0);
+(4, 2, 1, 1, 0),
+(4, 5, 2, 0, 1),
+(1, 5, 2, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -212,6 +228,13 @@ CREATE TABLE `stats` (
   `death` int(11) DEFAULT NULL,
   `points` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `stats`
+--
+
+INSERT INTO `stats` (`id_story`, `played`, `death`, `points`) VALUES
+(1, 7, 2, 7);
 
 -- --------------------------------------------------------
 
@@ -237,7 +260,7 @@ CREATE TABLE `stories` (
 
 INSERT INTO `stories` (`id_story`, `title`, `author`, `nbChapters`, `finished`, `date`, `hide`, `summary`, `nbrPoints`) VALUES
 (1, 'Les détectives', 'correcteur_admin', 22, 0, '2022-05-03', 0, 'Comme tous les lundis après les cours, vous et vos amis courrez en direction du parc pour y retrouver Monsieur Charles, un vieil homme qui n’est jamais à court d’histoires. Comme tous les lundis, vous pourrez prendre votre goûter en écoutant les aventures décrites par ce conteur hors-pair. Mais aujourd’hui, on a beau être lundi, Monsieur Charles n’est pas là. A vous d’élucider le mystère entourant la disparition du vieil homme…', 5),
-(2, 'xsz', 'correcteur_admin', 1, 0, '2022-05-10', 0, 'csz', 8);
+(4, 'L’île Maudite', 'correcteur_admin2', 6, 0, '2022-05-11', 1, 'Vous êtes le capitaine de l’équipage de pirates le plus redouté de l’histoire, et vous ne refusez jamais une aventure, surtout lorsqu’à la clef de celle-ci se trouve le plus gros butin qu’on puisse imaginer. Mais parviendrez-vous à déjouer tous les pièges de l’île Maudite et à vous en sortir sain et sauf ? ', 5);
 
 -- --------------------------------------------------------
 
@@ -258,7 +281,8 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id_usr`, `login_usr`, `password_usr`, `acces`) VALUES
 (1, 'correcteur', 'mdp_correcteur_1234', 'classique'),
-(2, 'correcteur_admin', 'mdp_correcteur_1234', 'admin');
+(2, 'correcteur_admin', 'mdp_correcteur_1234', 'admin'),
+(4, 'correcteur_admin2', 'mdp_correcteur_1234', 'admin');
 
 --
 -- Index pour les tables déchargées
@@ -291,19 +315,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `chapters`
 --
 ALTER TABLE `chapters`
-  MODIFY `id_chapter` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id_chapter` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT pour la table `stories`
 --
 ALTER TABLE `stories`
-  MODIFY `id_story` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_story` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_usr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_usr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Contraintes pour les tables déchargées
