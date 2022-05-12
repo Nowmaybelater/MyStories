fcfb137e9cc2467f1bdcb6f5b9bdb15ec55805ae
Tinuviel11 <vlorsery@ensc.fr>
2022-05-12 16:26:40 +0200

    commentaires story_reveal

--- story_reveal.php
+++ story_reveal.php
@@ -1,11 +1,12 @@
 <?php include("includes/header.php") ?>
 <?php include("includes/connect.php") ?>
+<!--Cette page permet de révéler l'histoire quand celle-ci a été cachée, et renvoie sur une page qui confirme qui en confirme la réalisation-->
 <main>
     <div id="backgroundConnexion">
 
         <?php
             $storyId=$_GET['id'];
-            //on suprime toutes les lignes faisant référence à cette histoire
+            //la requête permet de changer le booléen qui régit la visibilité de l'histoire dans la table stories
             $req="UPDATE stories SET hide='0' WHERE id_story=?";
             $requete = $bdd->prepare($req);
             $requete->execute(array($storyId));
@@ -20,7 +21,7 @@
         <br />
         <br />
         <br />
-        <div id="btn-lecture">
+<!--le bouton permet de revenir à la page précédente, c'est-à-dire la liste des histoires écrites par l'utilisateur et dont il peut consulter les statistiques, etc-->        <div id="btn-lecture">
             <a id="size-btn" class="btn btn-outline-dark" href="ListeHistoires.php" role="button"> Retour à la page précédente </a>
         </div>
     </div>
